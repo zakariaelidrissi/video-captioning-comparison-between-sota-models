5e05e43c89bb0cf1f9f4aaf571165650458bd947
Zakaria EL Iidrissi <zakariaelidrissi175@gmail.com>
2025-04-06 16:53:52 +0100

    add_file

--- code/_get_model_key.py
+++ code/_get_model_key.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Constants:
-    OPEN_AI_KEY: str = "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"
+    OPEN_AI_KEY: str = ""
     GEMINI_KEY: str = "AIzaSyCw2H_1B1c1o2sfnAzBlUKZvMQ1hfDltDY"
     CLAUDE_KEY: str = "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"
